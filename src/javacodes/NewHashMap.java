ae14fd79e2cd099fb681e11bf632a7ea5ff16dde
Malina <malinashakya72@gmail.com>
2024-04-26 16:35:48 +0545

    Contains method added in SwapFirstAndLast

---
+++ src/javacodes/NewHashMap.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javacodes;
+
+/**
+ *
+ * @author malin
+ */
+public class NewHashMap {
+
+    int capacity = 5;
+    public Object[] table;
+    public int size;
+
+    public NewHashMap() {
+        table = new Object[capacity];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int hash(Object obj) {
+        int index = Math.abs(obj.hashCode() % capacity);
+        return index;
+    }
+
+    public static class KeyValue {
+
+        Object key;
+        Object value;
+        KeyValue next;
+
+        public KeyValue(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public void put(Object key, Object value) {
+        int index = hash(key);
+        if (table[index] == null) {
+            table[index] = new KeyValue(key, value);
+            size++;
+        } else {
+            KeyValue current = (KeyValue) table[index];
+            while (current != null) {
+                if (current.key.equals(key)) {
+                    current.value = value;
+                    return;
+                }
+                if (current.next == null) {
+                    break;
+                }
+                current = current.next;
+            }
+            current.next = new KeyValue(key, value);
+            size++;
+        }
+    }
+
+    public void printAllElements() {
+        for (Object entry : table) {
+            if (entry != null) {
+                if (entry instanceof KeyValue) {
+                    KeyValue current = (KeyValue) entry;
+                    while (current != null) {
+                        System.out.println("Key: " + current.key + ", Value: " + current.value);
+                        current = current.next;
+                    }
+                } else {
+                    NewHashMap set = (NewHashMap) entry;
+                    set.printAllElements();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        NewHashMap map = new NewHashMap();
+        System.out.println("Is Empty? " + map.isEmpty());
+        map.put("Hello", 1);
+        map.put("World", 2);
+        map.put(456, "Hi");
+        map.put(123, "Namaste");
+        map.put(456, "Hiii");
+        map.put(56, "iii");
+        System.out.println(map.size());
+        map.put(46, "Heyyyi");
+        map.put(146, "Heyyyiss");
+
+        System.out.println("Size of the map: " + map.size());
+        System.out.println("Check purpose");
+        map.printAllElements();
+        System.out.println("Is Empty? " + map.isEmpty());
+    }
+}
--- src/javacodes/SwapFirstAndLast.java
+++ src/javacodes/SwapFirstAndLast.java
@@ -3,48 +3,60 @@ package javacodes;
 import java.util.Scanner;
 
 public class SwapFirstAndLast {
+
     public static void swap(char[] sentence) {
         int first = 0;
         int count = 0;
-        boolean Digit = false; 
-        for (int i = 0; i < sentence.length; i++) {
-            if (sentence[i] == ' ') {
+        boolean isDigit = false;
+        for (int i = 0; i <= sentence.length; i++) {
+            //short circuit
+            if (i == sentence.length || sentence[i] == ' ') {
                 int last = i - 1;
-                if (!Digit) { 
+                if (!isDigit) {
                     char temp = sentence[first];
                     sentence[first] = sentence[last];
                     sentence[last] = temp;
                 }
-                Digit = false;
+                isDigit = false;
                 first = i + 1;
                 count++;
             } else if (Character.isDigit(sentence[i])) {
-                Digit = true; 
+                isDigit = true;
             }
         }
-        int last = sentence.length - 1;
-        if (!Digit) {
-            char temp = sentence[first];
-            sentence[first] = sentence[last];
-            sentence[last] = temp;
-        }
-        count++; 
+
+        count++;
         System.out.println("Count of Words in Sentence: " + count);
         System.out.println(sentence);
     }
-    
-    public static boolean  contains(String paragraph, String characters)
-    {
-        return paragraph.toLowerCase().contains(characters.toLowerCase());
+
+    public static boolean contains(String string, String subString) {
+
+        int subStringLength = subString.length();
+        int stringLength = string.length();
+        int remainingLength = stringLength - subStringLength;
+        for (int i = 0; i < remainingLength; i++) {
+            boolean isFound = true;
+            for (int j = 0; j < subStringLength; j++) {
+                if (string.charAt(i + j) != subString.charAt(j)) {
+                    isFound = false;
+                    
+                }
+            }
+            if(isFound)
+            {
+                return true;
+            }
+        }
+        return false;
     }
+
     public static void main(String[] args) {
         System.out.println("Enter sentence:");
         Scanner sc = new Scanner(System.in);
         String sentence = sc.nextLine();
         swap(sentence.toCharArray());
-        System.out.println(contains(sentence,"hel"));
-        System.out.println(contains(sentence,"llo e"));
-        System.out.println(contains(sentence,"apple"));
-        
+        System.out.println(contains("Hello World", "llo"));
+
     }
 }
