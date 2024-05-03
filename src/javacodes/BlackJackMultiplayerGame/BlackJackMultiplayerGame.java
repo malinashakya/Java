fff9d24e5bcad622cf4f3fb8b024878bc1f2875e
Malina <malinashakya72@gmail.com>
2024-05-03 18:52:39 +0545

    Added BlackJack

--- src/javacodes/BlackJackMultiplayerGame/BlackJackMultiplayerGame.java
+++ src/javacodes/BlackJackMultiplayerGame/BlackJackMultiplayerGame.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import static javacodes.BlackJackMultiplayerGame.GameManager.getPlayer;
+import static javacodes.BlackJackMultiplayerGame.GameManager.playGame;
 import static javacodes.BlackJackMultiplayerGame.GameManager.selectGameCondition;
 import javacodes.BlackJackMultiplayerGame.gametype.GameType;
 
@@ -27,9 +28,7 @@ public class BlackJackMultiplayerGame {
             GameDataReader gameDataReader = new GameDataReader();
             List<GameRecord> gameRecords = gameDataReader.readGameRecords("game_history.txt");
             GameType gametype = selectGameCondition(scanner);
-            //key:String type, value: Player type
             HashMap<String, Player> players = new HashMap<>();
-            // Now, you should have the gameRecords and the game type selected
             for (GameRecord record : gameRecords) {
                 players.put(record.name, new Player(record.name, record.balance));
             }
@@ -39,17 +38,10 @@ public class BlackJackMultiplayerGame {
             System.out.println("Starting a new game.");
             System.out.println("Welcome to the game of Blackjack!");
             GameType gametype = selectGameCondition(scanner);
-
-            //Changed to List
             List<GameRecord> records = new ArrayList<>();
             List<Player> players = getPlayer(scanner);
             System.out.println("\nYou each have 100 points in your accounts.");
-
             playGame((ArrayList<Player>) players, records, gametype);
         }
-    }
-
-    private static void playGame(ArrayList<Player> arrayList, List<GameRecord> gameRecords, GameType gametype) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 }
