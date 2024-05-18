a7b615a048377d76a07b6fdf155820e1c33098a3
Malina <malinashakya72@gmail.com>
2024-05-18 13:19:42 +0545

    Added ClientServerChat-SocketProgramming

---
+++ src/ServerClientChat_SocketProgramming/Server.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ServerClientChat_SocketProgramming;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author malin
+ */
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+
+        try {
+            serverSocket = new ServerSocket(12345);
+            System.out.println("Server started and listening on port 12345");
+
+            clientSocket = serverSocket.accept();
+            System.out.println("Client connected");
+
+            bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            String message;
+            while ((message = bufferedReader.readLine()) != null) {
+                System.out.println("Client: " + message);
+                bufferedWriter.write("Echo: " + message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+                if (message.equalsIgnoreCase("BYE")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (serverSocket != null) serverSocket.close();
+                if (clientSocket != null) clientSocket.close();
+                if (bufferedReader != null) bufferedReader.close();
+                if (bufferedWriter != null) bufferedWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
